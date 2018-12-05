02cbb1e4dbe29d808492e2fd7c706a90b6692dfa
Olle Vahlin <16olva@ad.ssis.nu>
2018-12-05 09:06:34 +0100

    Succesfull first "file_get_html" + extra experiments and tests

--- index.php
+++ index.php
@@ -1,18 +1,26 @@
-<?php
-    string file_get_contents(https://www.aftonbladet.se/nyheter/a/J1eymR/forskarupprop-mot-kinesiska-omskolningslager)
-?>
-
+<?php require_once 'simple_html_dom.php'; ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>examples</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
-    <script src="main.js"></script>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>examples</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+        <script src="main.js"></script>
+    </head>
+    <body>
+        <?php
+            $htmlKod1 = file_get_html('https://www.aftonbladet.se/nyheter/a/J1eymR/forskarupprop-mot-kinesiska-omskolningslager');
+            foreach ($htmlKod1->find('._11S-G') as $class) {
+                echo $class->text();
+            }  
+
+            $htmlKod1 = file_get_html('https://www.aftonbladet.se/nyheter/a/J1eymR/forskarupprop-mot-kinesiska-omskolningslager');
+            foreach ($htmlKod1->find('.c-Cz1') as $class) {
+                $summering = $class->text();
+            }  
+        ?>
+        <p> <input type="hidden" name="type" value="<?php echo $summering; ?>"> </p>
+    </body>
 </html>
