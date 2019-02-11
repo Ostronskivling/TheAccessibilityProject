232a4e55de8fc218c46058a9fd12d2f6c24a1ba4
Ostronskivling <16olva@ad.ssis.nu>
2019-02-11 18:42:52 +0100

    UppdateCss

---
+++ style.php
@@ -0,0 +1,62 @@
+<?php
+    header("Content-type: text/css; charset: UTF-8");
+?>
+
+* {
+    margin: 0;
+    padding: 0;
+}
+
+/* index.php START */
+.Summering {
+    Background-color: brown;
+    color: aqua;
+    font-size: 30px;
+}
+
+#globalNav {
+    background-color: rgb(255, 192, 110);
+    width: 100%;
+    height: 200px;
+    float: left;
+}
+
+#globalNav input[type=text] {
+    padding: 10px;
+    padding-right: 50px;
+    padding-left: 13px;
+    margin-top: 100px;
+    position: absolute;
+    margin-left: auto;
+    margin-right: auto;
+    left: 0;
+    right: 0;
+    width: 350px;
+    font-size: 30px;
+    border: none;
+    border-radius: 10px;
+}
+
+#globalNav input[type=submit] {
+    padding: 10px;
+    padding-right: 13px;
+    padding-left: 13px;
+    margin-top: 100px;
+    position: absolute;
+    margin-left: auto;
+    margin-right: auto;
+    margin-right: 400px;
+    left: 0;
+    right: 0;
+    width: 130px;
+    font-size: 30px;
+    border: none;
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
+}
+/* index.php END */
+
+
+/* article.php START */
+    
+/* article.php END */
